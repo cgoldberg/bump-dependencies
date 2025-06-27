63aac002ac825f50fc4da4745828213891117946
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-06-26 22:22:29 -0400

    Get package versions from PyPI API instead of using pip

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # bump-deps
+
+- Corey Goldberg, 2025
+- License: MIT
+
+## Generates a new pyproject.toml file with updated package dependencies.
+
+- won't touch < or <= or > or >= or !=
+- only == or ~= or ===
+- doesn't handle all dependency specifiers supported by PEP508
+
+# Usage:
+```
+usage: bump_deps [-h] [--path PATH]
+
+options:
+  -h, --help   show this help message and exit
+  --path PATH  path to pyproject.toml
+```
+(if no path is specified, pyproject.toml in the current directory is used)
--- bump_deps.py
+++ bump_deps.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
-#
-# This script generates a new pyproject.toml file with updated package dependencies.
-# won't touch < or <= or > or >= or !=
-# Doesn't handle all dependency specifiers supported by PEP508
+# Corey Goldberg, 2025
+# License: MIT
 
-# Usage: `./bump_requirements --dir /path/to/pyproject.toml`
-# (if no path is specified, pyproject.toml in the current directory is used)
-
+"""Bump Python package dependencies in pyproject.toml"""
 
 import argparse
 import os
-import platform
 import re
-import shutil
-import subprocess
-import venv
 
+import requests
 import tomlkit
 from rich.console import Console
 
@@ -26,7 +19,7 @@ def get_dependency_operator(dependency_specifier):
     if not operators:
         operator = None
     elif len(operators) != 1 or any(x in dependency_specifier for x in illegal_chars):
-        print(f"can't handle complex dependency specifiers. not updating: {dependency_specifier}")
+        print(f"not updating: {dependency_specifier} (can't handle complex dependency specifiers)")
         operator = None
     else:
         operator = operators[0]
@@ -34,102 +27,78 @@ def get_dependency_operator(dependency_specifier):
 
 
 def get_dependencies_groups(data):
-    dependency_specifiers = data["project"].get("dependencies", [])
-    optional_dependency_groups = list(data["project"].get("optional-dependencies", {}).keys())
-    if not any((dependency_specifiers, optional_dependency_groups)):
+    """Map each dependency group name to a list of dependency specifiers"""
+    main_dependency_specifiers = list(data["project"].get("dependencies", []))
+    dependency_groups = dict(data["project"].get("optional-dependencies", {}))
+    if main_dependency_specifiers:
+        dependency_groups.update({".": main_dependency_specifiers})
+    if not dependency_groups:
         raise ValueError("no dependencies found")
-    return dependency_specifiers, optional_dependency_groups
+    return dependency_groups
 
 
-def replace_dependencies(dependency_specifiers, dependency_updates_map):
-    updated_dependencies = dependency_specifiers.copy()
+def update_dependencies(dependency_specifiers):
+    updated_dependency_specifiers = []
     for dependency_specifier in dependency_specifiers:
         operator = get_dependency_operator(dependency_specifier)
         if operator:
-            dep_name = re.split(operator, dependency_specifier)[0]
-            if dep_name in dependency_updates_map.keys():
-                old = f"{dep_name}{operator}{dependency_updates_map[dep_name][0]}"
-                new = f"{dep_name}{operator}{dependency_updates_map[dep_name][1]}"
-                print(f"updating {old} to {new}")
-                updated_dependencies = [new if item == old else item for item in updated_dependencies]
-    return updated_dependencies
+            dep_name = dependency_specifier.split(operator)[0]
+            new_dep_version = fetch_latest_package_version(dep_name)
+            if new_dep_version is not None:
+                new_dependency_specifier = f"{dep_name}{operator}{new_dep_version}"
+                if dependency_specifier != new_dependency_specifier:
+                    print(f"updating: {dependency_specifier} to {new_dependency_specifier}")
+                    updated_dependency_specifiers.append(new_dependency_specifier)
+                else:
+                    print(f"not updating: {dependency_specifier} (no new version available)")
+                    updated_dependency_specifiers.append(dependency_specifier)
+            else:
+                print(f"not updating: {dependency_specifier} (error retrieving version from pypi.org)")
+                updated_dependency_specifiers.append(dependency_specifier)
+        else:
+            print(f"not updating: {dependency_specifier} (no version specified)")
+            updated_dependency_specifiers.append(dependency_specifier)
+    return updated_dependency_specifiers
 
 
-def create_venv(venv_path):
+def fetch_latest_package_version(package_name):
+    url = f"https://pypi.org/pypi/{package_name}/json"
     try:
-        shutil.rmtree(venv_path)
-    except FileNotFoundError:
-        pass
-    venv.create(venv_path, with_pip=True)
-    if platform.system() == "Windows":
-        py_dir = "Scripts"
-    else:
-        py_dir = "bin"
-    python_path = os.path.join(venv_path, py_dir, "python")
-    return python_path
-
-
-def remove_venv(venv_path):
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print("error connecting to pypi.org")
+        return None
     try:
-        shutil.rmtree(venv_path)
-    except FileNotFoundError:
-        pass
+        response.raise_for_status()  # raise an exception for bad status codes
+    except requests.exceptions.HTTPError:
+        return None
+    return response.json()["info"]["version"]
 
 
-def run(pyproject_toml_path, venv_name):
+def run(pyproject_toml_path):
     console = Console()
     with console.status(""):
-        print(f"reading {pyproject_toml_path}")
+        print(f"reading {pyproject_toml_path}\n")
         try:
             with open(pyproject_toml_path) as f:
                 data = tomlkit.load(f)
         except FileNotFoundError:
-            exit("\nno pyproject.toml found. aborting")
-
+            exit("no pyproject.toml found. aborting")
         try:
-            dependency_specifiers, optional_dependency_groups = get_dependencies_groups(data)
+            dependency_group_map = get_dependencies_groups(data)
         except ValueError as e:
-            exit(f"\n{e}")
-
-        dependency_groups = ["."] + [f".[{group}]" for group in optional_dependency_groups]
-
-        print(f"creating virtual environment: {venv_name}")
-        python_path = create_venv(venv_name)
-
-        for group in dependency_groups:
-            if group == ".":
-                print("installing main dependencies from [project]")
+            exit(e)
+        updated_dependency_group_map = {
+            dependency_group: update_dependencies(dependency_specifiers)
+            for dependency_group, dependency_specifiers in dependency_group_map.items()
+        }
+        for dependency_group, dependency_specifiers in updated_dependency_group_map.items():
+            if dependency_group == ".":
+                data["project"]["dependencies"] = dependency_specifiers
             else:
-                print(f"installing {group} dependencies from [project.optional-dependencies]")
-            subprocess.run([python_path, "-m", "pip", "install", group], stdout=subprocess.DEVNULL)
-
-        print("finding outdated dependencies")
-        dependency_update_lines = subprocess.run(
-            [python_path, "-m", "pip", "list", "--outdated"],
-            capture_output=True,
-            text=True,
-        ).stdout.splitlines()[2:]
-
-        print(f"deleting virtual environment: {venv_name}")
-        remove_venv(venv_name)
-
-        if not dependency_update_lines:
-            exit("\nno dependency updates needed")
-
-        dependency_updates_map = {}
-        for line in dependency_update_lines:
-            splat = line.split()
-            dependency_updates_map[splat[0]] = (splat[1], splat[2])
-
-        data["project"]["dependencies"] = replace_dependencies(data["project"]["dependencies"], dependency_updates_map)
-        for group in optional_dependency_groups:
-            data["project"]["optional-dependencies"][group] = replace_dependencies(
-                data["project"]["optional-dependencies"][group], dependency_updates_map
-            )
-
+                data["project"]["optional-dependencies"][dependency_group] = dependency_specifiers
         with open(pyproject_toml_path, "w") as f:
             tomlkit.dump(data, f)
-
         print("\ngenerated new pyproject.toml with updated dependencies")
 
 
@@ -140,9 +109,8 @@ def main():
         default=os.path.join(os.getcwd(), "pyproject.toml"),
         help="path to pyproject.toml",
     )
-    parser.add_argument("--venv", default="temp_venv", help="temporary virtual env name")
     args = parser.parse_args()
-    run(args.path, args.venv)
+    run(args.path)
 
 
 if __name__ == "__main__":
